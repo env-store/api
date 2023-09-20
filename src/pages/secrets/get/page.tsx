be431b7b5da9d120edc304457e1c6af96b6d8c8e
Alexander Ng <alexng353@gmail.com>
2023-09-20 00:21:30 -0700

    feat: more pages

---
+++ src/pages/secrets/get/page.tsx
@@ -0,0 +1,5 @@
+export default async (request: Request) => {
+  // const { secret } = await request.json();
+
+  return <div>Secrets</div>;
+};
---
+++ src/pages/secrets/new/page.ts
@@ -0,0 +1,23 @@
+import { db } from "@app/db";
+import { secrets } from "@app/schema";
+import { z } from "zod";
+
+const newSecretBody = z.object({
+  allowed_keys: z.array(z.string()),
+  message: z.string(),
+  project_id: z.string().nullable(),
+});
+
+export default async (request: Request) => {
+  const body = newSecretBody.parse(request.body);
+
+  console.log(body);
+
+  // const res = await db.insert(secrets).values({
+  //   allowed_keys: body.allowed_keys,
+  //   message: body.message,
+  //   project_id: body.project_id,
+  // });
+
+  // return res;
+};
