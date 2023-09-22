e03116d44c3560d03b816752773486a9e9c2116d
Alexander Ng <alexng353@gmail.com>
2023-09-22 16:32:35 -0700

    feat: secrets/nwe

---
+++ src/pages/projects/variables.ts
@@ -0,0 +1,11 @@
+import { RouteWithParams } from "xirelta";
+import z from "zod";
+const GetProjectVariables = z.object({
+  projectId: z.string(),
+});
+
+export const POST: RouteWithParams<"/secrets/new"> = async (req) => {
+  const body = GetProjectVariables.parse(req.body);
+
+  return true;
+};
--- src/pages/secrets/new.tsx
+++ src/pages/secrets/new.tsx
@@ -1,5 +1,25 @@
+import { db } from "@app/db";
+import { secrets } from "@app/schema";
 import { RouteWithParams } from "xirelta";
+import { z } from "zod";
+
+const NewSecretBody = z.object({
+  message: z.string(),
+  allowed_keys: z.array(z.string()).transform((value) => JSON.stringify(value)),
+  project_id: z.string().nullable().optional().default(null),
+});
 
 export const POST: RouteWithParams<"/secrets/new"> = async (request) => {
-  return new Response("Hello, world!");
+  const body = NewSecretBody.parse(request.body);
+
+  const res = await db
+    .insert(secrets)
+    .values({
+      message: body.message,
+      allowed_keys: body.allowed_keys,
+      project_id: body.project_id,
+    })
+    .execute();
+
+  return JSON.stringify(res);
 };
